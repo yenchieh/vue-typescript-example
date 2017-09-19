db9385cb18f7bd59b8f6531b63b1668eeb0e1c56
Yen-Chieh Chen <jack08300@gmail.com>
2017-09-18 22:20:08 -0400

    initial commit

---
+++ src/component/Hello.vue
@@ -0,0 +1,29 @@
+<!-- src/components/Hello.vue -->
+
+<template>
+    <div>
+        <div class="greeting">Hello {{ number }}</div>
+        <slot name="top"></slot>
+        <slot name="bottom"></slot>
+        <div>Progress {{ test }} </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+
+    export default Vue.extend({
+        data() {
+            return {
+                number: 6
+            }
+        },
+        props: ['test']
+    });
+</script>
+
+<style>
+    .greeting {
+        font-size: 20px;
+    }
+</style>
